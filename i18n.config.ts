d938c32a4310232ce03aec98000719f7f49c0762
NP <78978618+pyraminx123@users.noreply.github.com>
2024-03-12 13:04:05 +0100

    detects device language

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,26 +1,36 @@
-import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
-import "intl-pluralrules";
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import 'intl-pluralrules';
+import * as RNLocalize from 'react-native-localize';
 
+// when adding a new language don't forget to add it to deviceLanguage
 const resources = {
-    en: {
-        translation: require('./translations/en.json'),
-    },
-    de: {
-        translation: require('./translations/de.json'),
-    },
-    es: {
-        translation: require('./translations/es.json'),
-    },
-    fr: {
-        translation: require('./translations/fr.json'),
-    }
+  en: {
+    translation: require('./translations/en.json'),
+  },
+  de: {
+    translation: require('./translations/de.json'),
+  },
+  es: {
+    translation: require('./translations/es.json'),
+  },
+  fr: {
+    translation: require('./translations/fr.json'),
+  },
 };
 
+const deviceLanguage = RNLocalize.findBestLanguageTag([
+  'de',
+  'en',
+  'es',
+  'fr',
+])?.languageTag;
+
 i18n.use(initReactI18next).init({
-    resources,
-    fallbackLng: "en",
-    interpolation: {
-        escapeValue: false,
-    },
+  resources,
+  lng: deviceLanguage,
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
 });
