282c62eb4a0987575686653a11d8f2ddb1385a1c
NP <78978618+pyraminx123@users.noreply.github.com>
2024-03-20 18:58:53 +0100

    properly animated flashcard

--- screens/FlashcardsScreen.tsx
+++ screens/FlashcardsScreen.tsx
@@ -1,35 +1,61 @@
 import React from 'react';
 
-import {View, Text, Pressable, StyleSheet, Button} from 'react-native';
-import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  runOnUI,
+} from 'react-native-reanimated';
 
 type FlashcardsProps = {
   word: string;
   definition: string;
 };
 
-// use pressable later for Flashcard
+// thanks https://blog.stackademic.com/creating-a-3d-animated-flip-card-component-in-react-native-with-reanimated-d67ba35193af
 const Flashcard = (props: FlashcardsProps) => {
-  const width = useSharedValue(100);
-
-  const handlePress = () => {
-    width.value = withSpring(width.value + 50);
+  const rotationF = useSharedValue(0);
+  const rotationB = useSharedValue(180);
+  const frontStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{perspective: 1000}, {rotateY: `${rotationF.value}deg`}],
+      position: 'absolute',
+    };
+  });
+  const backStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{perspective: 1000}, {rotateY: `${rotationB.value}deg`}],
+      position: 'absolute',
+    };
+  });
+  const onAnimate = () => {
+    'worklet';
+    if (rotationF.value === 180) {
+      rotationF.value = withTiming(0, {duration: 1000});
+      rotationB.value = withTiming(180, {duration: 1000});
+      return;
+    }
+    rotationF.value = withTiming(180, {duration: 1000});
+    rotationB.value = withTiming(360, {duration: 1000});
+  };
+  const animate = () => {
+    runOnUI(onAnimate)();
   };
 
   return (
-    <View style={styles.container}>
-      <Animated.View
-        style={{
-          width,
-          height: 100,
-          backgroundColor: 'violet',
-        }}
-      />
-      <Button onPress={handlePress} title="Click me" />
-    </View>
+    <Pressable onPress={animate} style={styles.pressBtn}>
+      <Animated.View style={[frontStyle, styles.card]}>
+        <Text style={styles.text}>{props.word}</Text>
+      </Animated.View>
+      <Animated.View style={[backStyle, styles.card]}>
+        <Text style={styles.text}>{props.definition}</Text>
+      </Animated.View>
+    </Pressable>
   );
 };
 
+// props need to be updated
 const FlashcardsScreen = () => {
   return (
     <View style={styles.container}>
@@ -52,8 +78,14 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     borderBlockColor: 'black',
     borderWidth: 3,
+    backfaceVisibility: 'hidden',
     justifyContent: 'center',
     alignItems: 'center',
+    position: 'relative',
+  },
+  pressBtn: {
+    width: 300,
+    height: 200,
   },
   text: {
     fontSize: 50,
