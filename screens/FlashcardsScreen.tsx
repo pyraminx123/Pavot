c044816c354534e63ec78adadd54d6294a9a7c7e
NP <78978618+pyraminx123@users.noreply.github.com>
2024-03-20 17:32:49 +0100

    added reanimated

--- screens/FlashcardsScreen.tsx
+++ screens/FlashcardsScreen.tsx
@@ -1,17 +1,64 @@
 import React from 'react';
 
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, Pressable, StyleSheet, Button} from 'react-native';
+import Animated, {useSharedValue, withSpring} from 'react-native-reanimated';
 
-const Flashcard = () => {
+type FlashcardsProps = {
+  word: string;
+  definition: string;
+};
+
+// use pressable later for Flashcard
+const Flashcard = (props: FlashcardsProps) => {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
     </View>
   );
 };
 
 const FlashcardsScreen = () => {
-  return <Flashcard />;
+  return (
+    <View style={styles.container}>
+      <Flashcard word="to swim" definition="nadar" />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    width: 300,
+    height: 200,
+    backgroundColor: 'lightblue',
+    borderRadius: 25,
+    borderBlockColor: 'black',
+    borderWidth: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 50,
+    color: 'black',
+  },
+});
 
 export default FlashcardsScreen;
