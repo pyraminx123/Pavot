5b07beaed30ebb07689d5f60a9be02fa6d64e11a
NP <78978618+pyraminx123@users.noreply.github.com>
2024-04-06 13:17:55 +0200

    major FlashcardsScreen.tsx changes

--- screens/FlashcardsScreen.tsx
+++ screens/FlashcardsScreen.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {View, Text, Pressable, StyleSheet} from 'react-native';
+import {View, Text, Pressable, StyleSheet, Dimensions} from 'react-native';
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
   runOnUI,
+  runOnJS,
 } from 'react-native-reanimated';
 import {
   GestureDetector,
@@ -13,13 +14,18 @@ import {
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
 
-type FlashcardsProps = {
+interface myWordObj {
   word: string;
   definition: string;
-};
+  key: number;
+}
 
-// thanks https://blog.stackademic.com/creating-a-3d-animated-flip-card-component-in-react-native-with-reanimated-d67ba35193af
-const Flashcard = (props: FlashcardsProps) => {
+const Flashcard = (props: {
+  word: string;
+  definition: string;
+  words: Array<myWordObj>;
+  setWords: React.Dispatch<React.SetStateAction<Array<myWordObj>>>;
+}) => {
   // Animation, turn card
   const rotationF = useSharedValue(0);
   const rotationB = useSharedValue(180);
@@ -50,33 +56,74 @@ const Flashcard = (props: FlashcardsProps) => {
   };
 
   // Gesture, swipe
+  const screenWidth: number = Dimensions.get('window').width;
+  const threshold: number = (screenWidth / 100) * 10;
   const isPressed = useSharedValue(false);
   const offset = useSharedValue({x: 0, y: 0});
+  const goBackDuration = useSharedValue(0);
+
   const swipeStyle = useAnimatedStyle(() => {
     return {
-      transform: [{translateX: offset.value.x}, {translateY: offset.value.y}],
+      transform: [
+        {
+          translateX: withTiming(offset.value.x, {
+            duration: goBackDuration.value,
+          }),
+        },
+        {
+          translateY: withTiming(offset.value.y, {
+            duration: goBackDuration.value,
+          }),
+        },
+      ],
     };
   });
-  const start = useSharedValue({x: 0, y: 0});
+
+  const handleSwipedWord = () => {
+    'worklet';
+    if (props.words.length > 1) {
+      runOnJS(props.setWords)(props.words.slice(1));
+      console.log(props.words);
+    } // handle ending, so that last card also comes back to correct position
+    // eventually update
+    goBackDuration.value = 300;
+    offset.value = {
+      x: 0,
+      y: 0,
+    };
+  };
+
   const pan = Gesture.Pan()
     .onBegin(() => {
       isPressed.value = true;
     })
     .onUpdate(e => {
+      goBackDuration.value = 0;
       offset.value = {
-        x: e.translationX + start.value.x,
-        y: e.translationY + start.value.y,
-      };
-    })
-    .onEnd(() => {
-      start.value = {
-        x: offset.value.x,
-        y: offset.value.y,
+        x: e.translationX,
+        y: e.translationY,
       };
     })
     .onFinalize(() => {
+      if (offset.value.x > threshold) {
+        console.log('Word known', offset.value.x);
+        handleSwipedWord();
+        return null;
+      } else if (offset.value.x < -threshold) {
+        console.log('Word unkown', offset.value.x);
+        handleSwipedWord();
+        return null;
+      } else {
+        goBackDuration.value = 300;
+        console.log('nothing');
+        offset.value = {
+          x: 0,
+          y: 0,
+        };
+      }
       isPressed.value = false;
     });
+
   const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
   return (
@@ -98,10 +145,43 @@ const Flashcard = (props: FlashcardsProps) => {
 // props need to be updated, maybe in the future add example sentences
 // use useState and useEffect to update props
 const FlashcardsScreen = () => {
+  const [words, setWords] = useState([
+    {word: 'nadar', definition: 'to swim', key: 0},
+    {word: 'trepar', definition: 'to climb', key: 1},
+    {word: 'comer', definition: 'to eat', key: 2},
+    {word: 'hola', definition: 'hello', key: 3},
+  ]);
   return (
+    // after make car before alreacy appear but text just not visible
     <View style={styles.container}>
       <GestureHandlerRootView>
-        <Flashcard word="to swim" definition="nadar" />
+        {words.map((wordObj, index) => {
+          if (index === 0) {
+            // set unique key
+            const nextWordObj =
+              words.length === 1
+                ? {word: '', definition: '', key: 4}
+                : words[index + 1];
+            return (
+              <>
+                <Flashcard
+                  key={nextWordObj.key}
+                  word={nextWordObj.word}
+                  definition={nextWordObj.definition}
+                  words={words}
+                  setWords={setWords}
+                />
+                <Flashcard
+                  key={wordObj.key}
+                  word={wordObj.word}
+                  definition={wordObj.definition}
+                  words={words}
+                  setWords={setWords}
+                />
+              </>
+            );
+          }
+        })}
       </GestureHandlerRootView>
     </View>
   );
@@ -109,8 +189,7 @@ const FlashcardsScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
-    width: '100%',
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -124,16 +203,19 @@ const styles = StyleSheet.create({
     backfaceVisibility: 'hidden',
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'relative',
   },
   pressBtn: {
     width: 300,
     height: 200,
+    zIndex: 2,
+    position: 'absolute',
+    left: -150,
+    top: -100,
   },
   text: {
     fontSize: 50,
     color: 'black',
   },
 });
-
+console.log(Dimensions.get('window').width);
 export default FlashcardsScreen;
