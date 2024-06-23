355ed64d08812897bf1d877536fd6b5432c79c8c
NP <78978618+pyraminx123@users.noreply.github.com>
2024-06-23 10:52:17 +0200

    Moved QuestionModal to a separate file and changed the props

--- screens/components/addDeck.tsx
+++ screens/components/addDeck.tsx
@@ -8,42 +8,7 @@ import {
   TextInput,
 } from 'react-native';
 import {createDeck} from '../handleData';
-
-// Define the QuestionModal component
-const QuestionModal = (props: {
-  question: string;
-  modalVisible: boolean;
-  setModalVisible: Function;
-  deckName: string;
-  setDeckName: Function;
-  onDeckAdded: Function;
-  folderName: string;
-}) => {
-  const onClose = async () => {
-    await props.setModalVisible(false);
-    await createDeck(props.deckName, props.folderName);
-    props.onDeckAdded();
-  };
-
-  return (
-    <Modal
-      visible={props.modalVisible}
-      onRequestClose={() => onClose()}
-      transparent={true}>
-      <View style={styles.modalView}>
-        <Text>{props.question}</Text>
-        <TextInput
-          value={props.deckName}
-          onChangeText={text => props.setDeckName(text)}
-          style={styles.textInput}
-        />
-        <Pressable onPress={() => onClose()}>
-          <Text>Close</Text>
-        </Pressable>
-      </View>
-    </Modal>
-  );
-};
+import QuestionModal from './QuestionModal';
 
 // Define the AddDeck component
 const AddDeck = (props: {onDeckAdded: Function, folderName: string}) => {
@@ -55,19 +20,25 @@ const AddDeck = (props: {onDeckAdded: Function, folderName: string}) => {
     setModalVisible(true);
   };
 
+  const onClose = () => {
+    setModalVisible(false);
+    createDeck(inputValue, props.folderName);
+    props.onDeckAdded();
+  };
+
   return (
     <View>
       <Pressable style={styles.container} onPress={openModal}>
         <Text style={styles.text}>+</Text>
       </Pressable>
       <QuestionModal
-        question={'Deck name?'}
+        question={'Create a new deck'}
+        placeholers={['Enter deck name']}
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
-        deckName={inputValue}
-        setDeckName={setInputValue}
-        onDeckAdded={props.onDeckAdded}
-        folderName={props.folderName}
+        values={[inputValue]}
+        setValues={[setInputValue]}
+        onClose={onClose}
       />
     </View>
   );
@@ -86,17 +57,6 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 25,
-  },
-  textInput: {
-    height: 30,
-    width: 100,
-  },
-  modalView: {
-    margin: 50,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    padding: 50,
-    borderRadius: 25,
   },
 });
 
--- screens/components/addFolder.tsx
+++ screens/components/addFolder.tsx
@@ -3,49 +3,10 @@ import {
   Pressable,
   Text,
   StyleSheet,
-  Modal,
   View,
-  TextInput,
 } from 'react-native';
+import QuestionModal from './QuestionModal';
 import {createFolder} from '../handleData';
-
-// Define the QuestionModal component
-const QuestionModal = (props: {
-  modalVisible: boolean;
-  setModalVisible: Function;
-  value: string;
-  setValue: Function;
-  onFolderAdded: Function;
-}) => {
-  const onClose = async () => {
-    await props.setModalVisible(false);
-    await createFolder(props.value);
-    props.onFolderAdded();
-  };
-
-  return (
-    <Modal
-      visible={props.modalVisible}
-      onRequestClose={() => onClose()}
-      transparent={true}>
-      <View style={styles.modalContainer}>
-        <View style={styles.modalView}>
-          <Text style={styles.question} >Create a new Folder</Text>
-          <TextInput
-            value={props.value}
-            onChangeText={text => props.setValue(text)}
-            style={styles.textInput}
-            placeholder='Enter folder name'
-            placeholderTextColor='rgba(0, 0, 0, 0.5)'
-          />
-          <Pressable onPress={() => onClose()} style={styles.closeBtn} >
-            <Text>Close</Text>
-          </Pressable>
-        </View>
-      </View>
-    </Modal>
-  );
-};
 
 // Define the AddFolder component
 const AddFolder = (props: {onFolderAdded: Function}) => {
@@ -57,17 +18,25 @@ const AddFolder = (props: {onFolderAdded: Function}) => {
     setModalVisible(true);
   };
 
+  const onClose = () => {
+    setModalVisible(false);
+    createFolder(inputValue);
+    props.onFolderAdded(); // eg fetches folders
+  };
+
   return (
     <View>
       <Pressable style={styles.folder} onPress={openModal}>
         <Text style={styles.text}>+</Text>
       </Pressable>
       <QuestionModal
+        question={'Create a new folder'}
+        placeholers={['Enter folder name']}
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
-        value={inputValue}
-        setValue={setInputValue}
-        onFolderAdded={props.onFolderAdded}
+        values={[inputValue]}
+        setValues={[setInputValue]}
+        onClose={onClose}
       />
     </View>
   );
@@ -85,39 +54,6 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 25,
-  },
-  question: {
-    fontSize: 20,
-    margin: 20,
-  },
-  textInput: {
-    height: 50,
-    width: 300,
-    backgroundColor: 'white',
-    borderRadius: 15,
-    padding: 10,
-  },
-  closeBtn: {
-    margin: 20,
-    backgroundColor: 'white',
-    width: 120,
-    height: 30,
-    borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-  },
-  modalView: {
-    backgroundColor: '#83B4E1',
-    alignItems: 'center',
-    borderRadius: 25,
-    width: 345,
-    height: 200,
   },
 });
 
